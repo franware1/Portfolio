e251f11bf07a8242070a56c5fd276d8ef3ac64fa
Francisco Vu <franvu225@outlook.com>
2026-02-12 10:49:41 -0500

    Installed react components
    npm install react react-dom
    added pages

---
+++ Portfolio/src/main.tsx
@@ -0,0 +1,22 @@
+import React, { StrictMode } from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import { Navigate, BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import About from "./pages/about.tsx";
+import Projects from "./pages/projects.tsx";
+import Contact from "./pages/contact.tsx";
+import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./styles/styles.css";
+import "bootstrap/dist/css/bootstrap.bundle.min.js";
+
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact-me" element={<Contact />} />
+        </Routes>
+      </Router>
+  </StrictMode>
+);
