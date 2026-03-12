63e5c00f00bc0d08e8e3e668544708f4b9294069
Francisco Vu <153341752+franware1@users.noreply.github.com>
2026-03-12 13:35:33 -0400

    styles and gitignore
    Added the structure to styles.css
    Added .json and .woff2 to gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Landing from './pages/landing'
 import About from './pages/about'
 import Projects from './pages/projects'
 import Blog from './pages/blog'
+import Contact from './pages/contact.tsx'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/blog" element={<Blog />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   )
 }
