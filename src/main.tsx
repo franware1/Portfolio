7465f9f2c801d1ac1f50e57aae4584e3fca37d43
Francisco Vu <franvu225@gmail.com>
2026-04-15 18:30:36 -0400

    Landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React, { StrictMode } from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
-import { Navigate, BrowserRouter as Router, Routes, Route, BrowserRouter} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx';
-import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./styles/styles.css";
-import "bootstrap/dist/css/bootstrap.bundle.min.js";
 
 
 createRoot(document.getElementById("root")!).render(
